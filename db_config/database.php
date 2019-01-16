ec694883c5e55c456a3842721aa55f22226656a6
lorenzo26 <benedict.bueno26@gmail.com>
2019-01-16 14:15:06 +0800

    Update database.php

--- db_config/database.php
+++ db_config/database.php
@@ -1,8 +1,8 @@
 <?php
-   	$host = "localhost"; 
-    $username = "root"; 
-    $password = ""; 
-    $db_name = "examination"; 
+   	$host = "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"; 
+    $username = "k05o0tierk86qyil"; 
+    $password = "x12ul9gzie74lesm"; 
+    $db_name = "h5vezikob3buyxee"; 
     $connection = mysqli_connect($host, $username, $password, $db_name);
     
 ?>
